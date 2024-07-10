44d7aaa79b99f0fd5ffb017e3c282becdd592151
lcho3878 <leech3878@naver.com>
2024-07-10 17:43:58 +0900

    Chore: 스토리브드 삭제 및 초기 세팅, 테스트 파일 생성

---
+++ WeatherAt/TestViewController.swift
@@ -0,0 +1,18 @@
+//
+//  TestViewController.swift
+//  WeatherAt
+//
+//  Created by 이찬호 on 7/10/24.
+//
+
+import UIKit
+
+final class TestViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Test Viewdidload")
+        view.backgroundColor = .systemBackground
+    }
+    
+}
