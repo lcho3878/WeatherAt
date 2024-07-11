bd4ee7bb8e6c940f8bcce71038e66a117534af28
lcho3878 <leech3878@naver.com>
2024-07-11 18:18:54 +0900

    Add: Observable class 생성

---
+++ WeatherAt/Observable.swift
@@ -0,0 +1,31 @@
+//
+//  Observable.swift
+//  WeatherAt
+//
+//  Created by 이찬호 on 7/11/24.
+//
+
+import Foundation
+
+class Observable<T> {
+    
+    typealias Listener = (T) -> Void
+    
+    var listener: Listener?
+    
+    var value: T {
+        didSet{
+            listener?(value)
+        }
+    }
+    
+    init(value: T) {
+        self.value = value
+    }
+    
+    func bind(listener: Listener?) {
+        self.listener = listener
+        listener?(value)
+    }
+    
+}
